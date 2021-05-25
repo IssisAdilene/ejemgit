a334e2e176c2cab315dfebd8e7d7e61126dc3dab
IssisAdilene <issisadilene03@gmail.com>
2021-05-25 15:34:09 -0600

    mi primer commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>SASUKEEEEEEEE!</h1>
+    <div>
+      <img src="https://i.pinimg.com/736x/54/cc/ec/54ccecec9651821853506cd5743c5086.jpg" height="450px" width="450px">
+    </div>
+  </div>
+</template>
